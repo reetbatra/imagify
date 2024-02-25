f0459dade10f5b1c0b067f158dfef4a5644ca63f
reetbatra <reetbatra25@gmail.com>
2024-02-25 16:23:03 +0530

    adding middleware, sign-in and sign-up using clerk

---
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,11 @@
+import { SignIn, SignUp } from '@clerk/nextjs'
+import React from 'react'
+
+const SignInPage = () => {
+  return 
+    <SignIn />
+}
+
+
+
+export default SignInPage
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Sidebar } from 'lucide-react'
 
 const Layout = ({children}: {children: React.ReactNode}) => {
   return (
     <main className="root">
+      {/* Sidebar */}
+      <Sidebar/>
+       {/* Navbar */}
         <div className="root-container">
             <div className="wrapper">
                 {children}
